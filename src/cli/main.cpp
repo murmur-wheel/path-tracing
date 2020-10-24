bfa1b33fc8900cb9acb611f75fa0091d0adfe345
murmur-wheel <murmur.wheel@gmail.com>
2020-10-24 10:45:38 +0800

    add cli

---
+++ src/cli/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by murmur-wheel on 2020/10/24.
+//
+
+#include <glog/logging.h>
+
+int main(int argc, char* argv[]) {
+  ::google::InitGoogleLogging(argv[0]);
+  LOG(INFO) << "hello, glog";
+}
