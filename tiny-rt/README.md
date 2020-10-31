733d9234226a6eb5797ddd2bcf9a469a62e35cab
murmur-wheel <murmur.wheel@gmail.com>
2020-10-31 11:04:43 +0800

    checkerboard

--- tiny-rt/README.md
+++ tiny-rt/README.md
@@ -1,6 +1,6 @@
 # tiny rt
 
-用来展示 ray-tracing 算法原理的渲染器，类似于 [smallpt](http://www.kevinbeason.com/smallpt/)，特性有：
+用来展示 ray-tracing 算法原理的渲染器，类似于 [smallpt](http://www.kevinbeason.com/smallpt/)，实现了如下特性：
 
 - Global illuination
 - Specualr, Diffuse and Glass BRDFs
--- tiny-rt/main.cpp
+++ tiny-rt/main.cpp
@@ -1,6 +1,10 @@
 #include <cmath>
 #include <cstdlib>
+#include <string>
 #include <vector>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 using Float = float;
 
@@ -71,6 +75,24 @@ Vec3 sqrt(const Vec3& v) {
   return v * inv;
 }
 
+Vec3 normalize(const Vec3& v) {
+  Float l = std::sqrt(dot(v, v));
+  Float inv = 1.0 / l;
+  return v * inv;
+}
+
+Float clamp(Float t, Float low, Float high) {
+  if (t < low) return low;
+  if (t > high) return high;
+  return t;
+}
+
+void store(const Vec3& v, uint8_t buf[3]) {
+  buf[0] = int32_t(clamp(v.x, 0, 1) * 255.f);
+  buf[1] = int32_t(clamp(v.y, 0, 1) * 255.f);
+  buf[2] = int32_t(clamp(v.z, 0, 1) * 255.f);
+}
+
 struct Ray {
   Vec3 origin;
   Vec3 direction;
@@ -78,23 +100,58 @@ struct Ray {
 
 struct Material {
   Vec3 emissive;
-  Vec3 Kd;  // diffuse factor
-  Vec3 ks;  // specular factor
+  Vec3 color;  // base color
+  Vec3 Kd;     // diffuse factor
+  Vec3 ks;     // specular factor
 };
 
-struct Sphere {
-  Vec3 center;
-  Float radius = 0;
+struct HitInfo {
+  Vec3 position;
+  Vec3 normal;
+  Material* material = nullptr;
+};
 
-  const Material* material = nullptr;
-
+class Sphere {
+ public:
   Sphere() = default;
-  explicit Sphere(const Vec3& _center, Float _radus, const Material* _material)
-      : center(_center), radius(_radus), material(_material) {}
+  explicit Sphere(const Vec3& _center, Float _radius, Material* _material)
+      : center_(_center), radius_(_radius), material(_material) {}
+
+  virtual bool intersects(const Ray& ray, Float* t_hit,
+                          HitInfo* hit_info) const {
+    auto v = ray.origin - center_;
+    Float a = dot(ray.direction, ray.direction);
+    Float b = 2 * dot(v, ray.direction);
+    Float c = dot(v, v) - radius_ * radius_;
+    Float D = b * b - 4 * a * c;
+    if (D < 0) {
+      return false;
+    }
+
+    Float d = std::sqrt(D);
+    Float t = (b - d) / (2 * a);
+    if (t < 0) {
+      t = (b + d) / (2 * a);
+    }
+
+    *t_hit = t;
+    if (hit_info) {
+      hit_info->position = ray.origin + ray.direction * t;
+      hit_info->normal = normalize(hit_info->position - center_);
+      hit_info->material = material;
+    }
+
+    return true;
+  }
+
+ private:
+  Vec3 center_;
+  Float radius_ = 0;
+  Material* material = nullptr;
 };
 
 struct Scene {
-  std::vector<Sphere*> shapes;
+  std::vector<Sphere*> spheres;
 };
 
 struct PerspectiveCamera {
@@ -107,17 +164,31 @@ struct PerspectiveCamera {
   void generate_ray(float film_u, float film_v, Ray* ray) const {}
 };
 
-void setup_camera() {}
-void setup_scene() {}
-void render() {}
-void save_result() {}
+void setup_camera(PerspectiveCamera* camera) {}
+void setup_scene(Scene* scene) {}
+void render(const PerspectiveCamera* camera, const Scene* scene, int width,
+            int height, const std::string& output) {
+  uint8_t* buf = new uint8_t[640 * 480 * 3];
+
+  for (int x = 0; x < width; ++x) {
+    for (int y = 0; y < height; ++y) {
+      Vec3 color;
+      int grid_x = x / 32, grid_y = y / 32;
+      if ((grid_x + grid_y) % 2) {
+        color = Vec3(1);
+      }
+      store(color, buf + (x + y * width) * 3);
+    }
+  }
+
+  stbi_write_png(output.c_str(), width, height, 3, buf, width * 3);
+  delete[] buf;
+}
 
 int main(int argc, char* argv[]) {
   PerspectiveCamera camera;
+  setup_camera(&camera);
   Scene scene;
-
-  setup_camera();
-  setup_scene();
-  render();
-  save_result();
+  setup_scene(&scene);
+  render(&camera, &scene, 640, 480, "color.png");
 }
